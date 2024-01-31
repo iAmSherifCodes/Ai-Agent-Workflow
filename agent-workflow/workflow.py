4d85d1b6a45fca73002b9b1a1775bbf78cb67a90
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-01-31 16:56:21 +0100

    Homekeeping

--- agent-workflow/workflow.py
+++ agent-workflow/workflow.py
@@ -16,29 +16,27 @@ examiner= examiner_agent(True,False,[search_tool],ollama_llm)
 topic= input("Enter your subject:  ")
 
 task1 = Task(
-    description=f'Conduct a detailed exploration of what {topic} is, delving into specific areas that can be '
-                'presented in an informative and engaging manner to a broad audience',
-    # description='Develop ideas for teaching someone new to the technology',
-    agent=researcher,
-)
+            description=f'Conduct a detailed exploration of what {topic} is, delving into specific areas that can be '
+                        'presented in an informative and engaging manner to a broad audience',
+            agent=researcher,
+        )
 
 task2 = Task(
-    description=f'Create a 3 paragraphs long blog post about introduction to {topic} using your insights. Make it interesting, '
-                         'clear and suited for everyone including a non-tech person.',
-    # description=f"Use the Researcher's ideas to write a piece of text to explain the {topic}",
-             agent=writer)
+            description=f'Create a 3 paragraphs long blog post about introduction to {topic} using your insights. Make it interesting, '
+                                'clear and suited for everyone including a non-tech person.',
+            agent=writer
+        )
 
 task3 = Task(
-    # description="""
-            # Create 2-3 test questions to assess the reader's understanding of the written content on the explored technology concepts""",
              description="Craft 2-3 test questions to evaluate understanding of the created text, along with the correct answers",
-             agent=examiner)
+             agent=examiner
+            )
 
 crew = CrewaiCrew(
-    agents=[researcher, writer, examiner],
-    tasks=[task1, task2, task3],
-    verbose=2
-)
+            agents=[researcher, writer, examiner],
+            tasks=[task1, task2, task3],
+            verbose=2
+        )
 
 
 
